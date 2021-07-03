eb626656be830a814096698241db11493968d7f1
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-03 19:08:13 +0300

    update SQL scheme [parser]

--- parser/scheme.sql
+++ parser/scheme.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS messages (
 	id SERIAL PRIMARY KEY,	
 	data json,	
 	created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-	imagepath text
+	imagepath text,
+	tg_id integer unique
 );
 
 ALTER TABLE messages OWNER TO [user];
