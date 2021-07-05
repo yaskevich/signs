a9f61496680d264ee4b156dc799075bf7c0e6fc1
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-05 18:01:09 +0300

    enable PrimeVue UI [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-orange/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue)
+  .mount('#app')
