1ef4daa7694805e695404eeab5f5f0b40b08400d
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-06 23:55:29 +0300

    render second instance of Paginator in Messages [client]

--- client/src/views/Messages.vue
+++ client/src/views/Messages.vue
@@ -3,7 +3,7 @@
   <div class="about">
     <!-- <h1>Data</h1> -->
   </div>
-  <Paginator :rows="pageRows" :totalRecords="pageCount" :rowsPerPageOptions="pageOptions" @page="onPage($event)"></Paginator>
+  <Paginator :first="pageOffset" :rows="pageRows" :totalRecords="pageCount" :rowsPerPageOptions="pageOptions" @page="onPage($event)"></Paginator>
   <div style="text-align:left; margin:auto;">
     <div v-for="(value, key) in messagesPage" :key="key" class="p-shadow-1 p-p-3 p-d-flex card" style="">
       <div class="p-mr-6" style="">
@@ -32,6 +32,17 @@
             <td style="font-weight:bold;">Content</td>
             <td style="border:1px dashed red;"><span v-html="value.data.message?.split('\n').join('<br/>')"></span></td>
           </tr>
+          <tr v-if="value.data.grouped_id">
+            <td style="font-weight:bold;">Group</td>
+            <td>{{value.data["grouped_id"]}}</td>
+          </tr>
+          <tr v-if="value.data.fwd_from">
+            <td style="font-weight:bold;">Fwd</td>
+            <td>
+              {{value.data.fwd_from.from_id?.channel_id}}/{{value.data.fwd_from?.channel_post}}
+              <br/>{{value.data.fwd_from.date}}
+            </td>
+          </tr>
           <!-- <tr>
               <td></td>
               <td>
@@ -41,7 +52,7 @@
       </div>
       <div class="p-ml-auto">
         <div class="p-text-right p-mb-2">
-          <Button label="Annotate" class="p-button-outlined p-button-secondary" />
+          <Button label="Annotate" class="p-button-outlined p-button-secondary" @click="goToMessage(value.tg_id)" v-if="value.imagepath && value.data['_'] == 'Message'"/>
         </div>
         <div>
           <img :src="'/api/media/thumbs/'+value.imagepath" v-if="value.imagepath" />
@@ -49,40 +60,49 @@
       </div>
     </div>
   </div>
+  <Paginator :first="pageOffset" :rows="pageRows" :totalRecords="pageCount" :rowsPerPageOptions="pageOptions" @page="onPage($event)"></Paginator>
 
 </template>
 <script>
 
   import { reactive, ref, onMounted } from 'vue';
+  import router from "../router";
   import axios from 'axios';
   export default {
     name: 'Messages',
     setup() {
       const messages = ref([]);
       const users = ref({});
-      const pageRows = 25;
-      const pageOptions = [25, 100, 500];
+      const pageRows = ref(25);
+      const pageOffset = ref(0);
+      const pageOptions = [10, 25, 50, 100];
       const pageCount = ref(0);
 
       const messagesPage = ref([]);
       onMounted(async () => {
-        const { data } = await axios.get('/api/messages', { params: { off: 0, batch: pageRows } });
+        const { data } = await axios.get('/api/messages', { params: { off: 0, batch: pageRows.value } });
         messages.value = data.data;
         pageCount.value = Number(data.count);
         users.value = data.users;
         // console.log(data.users);
-        messagesPage.value = messages.value.slice(0, pageRows);
+        messagesPage.value = messages.value.slice(0, pageRows.value);
         // console.log(messages.value);
       });
 
       const onPage = async e => {
-        // console.log(e);
+        console.log(e);
         // console.log(messages.value.length, e.first);
         // messagesPage.value = messages.value.slice(e.first, e.first+ e.rows);
+        pageOffset.value = e.first;
+        pageRows.value = e.rows;
         const { data } = await axios.get('/api/messages', { params: { off: e.first, batch: e.rows } });
         messagesPage.value = data.data;
       };
-      return { messages, messagesPage, onPage, pageRows, pageOptions, users, pageCount };
+
+      const goToMessage = (id) => {
+        router.push('/message/' + id);
+      }
+      return { messages, messagesPage, onPage, pageRows, pageOptions, pageOffset, users, pageCount, goToMessage, };
     },
     components: {},
   };
