0ebbaf210575f66897e3718e08388c3c1d281f7a
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-07 22:26:26 +0300

    render stats in Home [client]

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <h1><span class="p-shadow-9 p-p-2">Signs project</span></h1>
@@ -6,17 +7,58 @@
     <p>
       Click <b>Posts</b> link in page header
     </p>
+    <Divider />
+    <h4>Stats</h4>
+    <div class="p-grid">
+      <div class="p-col">Total Messages</div>
+      <div class="p-col">{{datum?.messages}}</div>
+    </div>
+
+    <div class="p-grid">
+      <div class="p-col">...with Photo</div>
+      <div class="p-col">{{datum?.photos}}</div>
+    </div>
+
+    <div class="p-grid">
+      <div class="p-col">...with Annotation</div>
+      <div class="p-col p-text-bold" style="color:red;">{{datum?.annotations}}</div>
+    </div>
+
+    <h5>Languages</h5>
+    <div v-for="(value, key) in datum?.scheme?.languages" :key="key">
+      <div class="p-grid">
+        <div class="p-col">{{value.replace('TAG-', '')}}</div>
+        <div class="p-col p-text-bold" style="color:orange;">{{datum?.languages[key]}}</div>
+      </div>
+    </div>
+
+    <h5>Features</h5>
+    <div v-for="(value, key) in datum?.scheme?.features" :key="key">
+      <div class="p-grid">
+        <div class="p-col">{{value.replace('TAG-', '')}}</div>
+        <div class="p-col p-text-bold" style="color:gray;">{{datum?.features[key]}}</div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  import { defineComponent, onBeforeMount, ref, reactive } from 'vue';
+  import axios from 'axios';
+  export default defineComponent({
+    setup() {
+      const datum = ref({});
+
+      onBeforeMount(async () => {
+        const { data } = await axios.get('/api/stats');
+        datum.value = data;
+        console.log('stats', data);
+      });
+
+      return { datum };
+    },
+  });
+
 </script>
