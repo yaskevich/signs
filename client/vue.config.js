73d625d71457689beba1df72f7805a83e1b3a064
yaskevich <alyaxey.yaskevich@gmail.com>
2022-03-18 18:58:54 +0300

    update Webpack config [client]

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,40 +1,31 @@
 //vue.config.js
 const webpack = require('webpack');
 module.exports = {
-	pages: {
+  pages: {
     index: {
       entry: 'src/main.ts',
-			// the source template
+      // the source template
       template: 'public/index.html',
       // output as dist/index.html
       filename: 'index.html',
       title: 'Signs'
     }
   },
-	configureWebpack: {
-		plugins: [
-			// Define Bundler Build Feature Flags
-			new webpack.DefinePlugin({
-				// Drop Options API from bundle
-				"__VUE_OPTIONS_API__": false,
-				"__VUE_PROD_DEVTOOLS__": false,
-			}),
-		]},
-	devServer: {
-		port: 3040,
-		host: "localhost",
-		https: false,
-		open: false,
-		proxy: {
-			"/api": {
-				target: "http://localhost:3041",
-				changeOrigin: true,
-				ws:true,
-				// pathRewrite: {
-					// "^/api": ""
-				// },
-				logLevel: 'debug'
-			},
-		}
-	}
+  devServer: {
+    port: 3040,
+    host: "localhost",
+    https: false,
+    open: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3041",
+        changeOrigin: true,
+        ws:true,
+        // pathRewrite: {
+          // "^/api": ""
+        // },
+        logLevel: 'debug'
+      },
+    }
+  }
 }
