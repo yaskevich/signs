5821169aed5afe0bd88de929a1673af8aeb7cffd
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-08-08 13:07:58 +0300

    restyle Home [client]

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,47 +1,32 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <h1>
-      <span class="shadow-9 2">Signs project</span>
-    </h1>
-    <h3>¡signs are everywhere!</h3>
-    <p>
-      Click
-      <b>Posts</b> link in page header
-    </p>
-    <n-divider></n-divider>
-    <div v-if="isLoaded">
-      <h4>Stats</h4>
-      <div class="grid">
-        <div class="col">Total Messages</div>
-        <div class="col">{{ datum?.messages }}</div>
-      </div>
-
-      <div class="grid">
-        <div class="col">...with Photo</div>
-        <div class="col">{{ datum?.photos }}</div>
-      </div>
-
-      <div class="grid">
-        <div class="col">...with Annotation</div>
-        <div class="col font-bold" style="color:rgb(255, 0, 162);">{{ datum?.annotations }}</div>
-      </div>
+    <div v-if="isLoaded" style="margin:auto;text-align:center;">
+      
+      <n-descriptions label-placement="top" title="Stats" label-align="center">
+        <n-descriptions-item>
+          <template #label>Total Messages</template>
+          {{ datum?.messages }}
+        </n-descriptions-item>
+        <n-descriptions-item label="...with Photo">{{ datum?.photos }}</n-descriptions-item>
+        <n-descriptions-item label="...with Annotation">
+          <span style="color:rgb(255, 0, 162);font-weight:bold;">{{ datum?.annotations }}</span>
+        </n-descriptions-item>
+      </n-descriptions>
 
       <h5>Languages</h5>
-      <div v-for="(value, key) in datum?.scheme?.languages" :key="key">
-        <div class="grid">
-          <div class="col">{{ value.replace('TAG-', '') }}</div>
-          <div class="col font-bold" style="color:orange;">{{ datum?.languages[key] }}</div>
-        </div>
-      </div>
+      <n-grid v-for="(value, key) in datum?.scheme?.languages" :key="key" x-gap="12" :cols="2">
+        <n-gi>{{ value.replace('TAG-', '') }}</n-gi>
+        <n-gi style="color:orange;">{{ datum?.languages[key] }}</n-gi>
+      </n-grid>
 
       <h5>Features</h5>
-      <div v-for="(value, key) in datum?.scheme?.features" :key="key">
-        <div class="grid">
-          <div class="col">{{ value.replace('TAG-', '') }}</div>
-          <div class="col font-bold" style="color:gray;">{{ datum?.features[key] }}</div>
-        </div>
-      </div>
+      <n-grid v-for="(value, key) in datum?.scheme?.features" :key="key" x-gap="12" :cols="2">
+        <n-gi>{{ value.replace('TAG-', '') }}</n-gi>
+        <n-gi style="color:gray;">{{ datum?.features[key] }}</n-gi>
+      </n-grid>
+    </div>
+    <div v-else style="text-align:center">
+      ...loading
     </div>
   </div>
 </template>
