33f9d683e3bb8db289a87bc62fb2e681267fa4ee
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-08-08 13:14:30 +0300

    update router [client]

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory, RouteRecordRaw, RouterScrollBehavior,   } from 'vue-router'
-import Home from '../views/Home.vue'
-import TMessages from '../views/TMessages.vue'
-import TMessage from '../views/TMessage.vue'
+import { createRouter, createWebHistory, RouteRecordRaw, RouterScrollBehavior, } from 'vue-router'
+import Home from '../components/Home.vue'
+import TMessages from '../components/TMessages.vue'
+import TMessage from '../components/TMessage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -20,32 +20,39 @@ const routes: Array<RouteRecordRaw> = [
     component: TMessages
   },
   {
-		path: '/message/:id?',
-		name: 'TMessage',
-		component: TMessage
-	},
+    path: '/message/:id?',
+    name: 'TMessage',
+    component: TMessage
+  },
   {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/About.vue')
   }
 ]
 
-const scrollBehavior: RouterScrollBehavior = (to:any, from:any, savedPosition:any) => {
-  if (savedPosition) {
-    return savedPosition
-  } else {
-    return { x: 0, y: 0 }
-  }
+const scrollBehavior: RouterScrollBehavior = async (to: any, from: any, savedPosition: any) => {
+  console.log("savedPosition", savedPosition);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (savedPosition) {
+        console.log("restore scroll position");
+        resolve(savedPosition);
+      } else {
+        resolve({ top: 0 });
+      }
+    }, 1500)
+  })
 };
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
   scrollBehavior
-})
+});
 
 export default router
