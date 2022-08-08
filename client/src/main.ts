83e54a17a9cacb5915daa35b071e62bdfff42c56
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-08-08 13:11:23 +0300

    enable auto-imports [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,30 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import 'primeflex/primeflex.css';
-import 'primevue/resources/themes/fluent-light/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
 
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import Divider from 'primevue/divider';
-import Dropdown from 'primevue/dropdown';
-import ScrollTop from 'primevue/scrolltop';
-import Paginator from 'primevue/paginator';
-import SelectButton from 'primevue/selectbutton';
-import Message from 'primevue/message';
-
-const app  = createApp(App);
-app.component('ScrollTop', ScrollTop);
-app.component('Dropdown', Dropdown);
-app.component('InputText', InputText);
-app.component('Button', Button);
-app.component('SelectButton', SelectButton);
-app.component('Divider', Divider);
-app.component('Paginator', Paginator);
-app.component('Message', Message);
-
-
-app.use(router).use(PrimeVue).mount('#app')
+createApp(App).use(router).mount('#app');
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Checker from 'vite-plugin-checker'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Checker({ typescript: true })],
+  plugins: [
+    vue(), Checker({ typescript: true }), AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+        },
+      ],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
   server: {
     port: 3040,
     proxy: {
-     '/api': {
-       target: 'http://localhost:3041',
-       changeOrigin: true,
-       secure: false,
-       ws: true,
-     }
-   }
+      '/api': {
+        target: 'http://localhost:3041',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
   }
 })
