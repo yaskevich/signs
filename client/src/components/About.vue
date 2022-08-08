76cc1e8bc2c749f0ec7f86786a70587907bd9de0
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-08-08 13:09:16 +0300

    update About [client]

---
+++ client/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div class="about" style="text-align:center">
+    <!-- <h1>This is an about page</h1>  -->
+     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h2>
+      <span class="shadow-9 2">Signs project</span>
+    </h2>
+    <h3>¡signs are everywhere!</h3>
+    <p>
+      Click
+      <b>Posts</b> link in page header
+    </p>
+    <n-divider></n-divider>
+  </div>
+</template>
