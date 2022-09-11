45dd414357222171d8214be2e8487919b41f9708
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-11 14:55:25 +0300

    add declaration for TiltedBox [client]

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -1,12 +1,14 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
-declare module "@recogito/annotorious" {
- export class Annotorious {
+declare module '@recogito/annotorious' {
+  export class Annotorious {
     constructor(T: Object | null);
+  }
 }
-
+declare module '@recogito/annotorious-tilted-box' {
+  export default function TiltedBoxPlugin(T:Object):any
 }
