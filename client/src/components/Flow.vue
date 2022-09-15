be5261d78714cf64be8f814d18ff0da124e8e0d3
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-15 22:26:46 +0300

    render NSpin on loading in Flow [client]

--- client/src/components/Flow.vue
+++ client/src/components/Flow.vue
@@ -1,85 +1,82 @@
 <template>
-  <n-card :title="`All annotations (${totalCount})`">
-    <n-pagination
-      v-model:page="page"
-      v-model:page-size="pageSize"
-      show-size-picker
-      :item-count="totalCount"
-      :page-sizes="[10, 50, 100, 250, 500, 1000]"
-      @update:page="changePage"
-      @update:page-size="changePageSize"
-    />
-    <n-divider></n-divider>
-    <n-space vertical size="large" v-show="isLoaded">
-      <div v-for="(item, index) in items" :key="index" class="photo">
-        <div
-          v-for="anno in item?.annotations?.map(x => processAnnotations(x, item.tg_id))"
-          :key="anno.id"
-          class="anno"
-          v-bind:class="{ error: anno.error }"
-        >
-          <!-- <span>{{ anno }}}</span> -->
-          <n-space vertical>
-            <n-h3 prefix="bar" type="info" style="margin-bottom: -1px">{{ anno?.heading }}</n-h3>
-            <n-space>
-              <template v-for="tag in anno?.tags">
-                <n-button-group size="small" v-if="tag?.title">
-                  <n-button type="info" size="small">{{ tag.label }}</n-button>
-                  <n-tooltip trigger="hover">
-                    <template #trigger>
-                      <n-button size="small" color="#2080f0">
-                        <template #icon>
-                          <n-icon :component="InfoCircle" />
-                        </template>
-                      </n-button>
-                    </template>
-                    {{ tag.title }}
-                  </n-tooltip>
-                </n-button-group>
-                <template v-else>
-                  <n-button v-if="tag?.language" color="#5a428d" size="small">{{ tag.label }}</n-button>
-                  <n-button v-else type="info" size="small">{{ tag.label }}</n-button>
-                </template>
-              </template>
-            </n-space>
-            <!-- <n-space>
-                        <n-button @click="$router.push(`/message/${item?.tg_id}`)">{{ item?.tg_id }}</n-button>
-                        <template v-for="(feature, n) in anno.body">
-                            <div v-if="feature.purpose === 'commenting'">
-                                <n-h6 align-text>{{ feature.value }}</n-h6>
-                            </div>
-
-                            <n-tag
-                                type="success"
-                                v-if="feature.purpose === 'tagging'"
-                            >{{ feature.value.replace('TAG-', '') }}</n-tag>
-                            <span
-                                v-if="!['commenting', 'tagging'].includes(feature.purpose)"
-                            >{{ feature }}</span>
-                        </template>
-                        </n-space>-->
+  <n-card>
+    <template #header>
+      <n-spin :show="!isLoaded"> All annotations ({{ totalCount }}) </n-spin>
+    </template>
+    <n-space vertical size="large" v-if="isLoaded">
+      <n-pagination
+        v-model:page="page"
+        v-model:page-size="pageSize"
+        show-size-picker
+        :item-count="totalCount"
+        :page-sizes="paginationOptions"
+        @update:page="changePage"
+        @update:page-size="changePageSize"
+      />
+      <div style="min-height: 400px">
+        <n-space vertical size="large">
+          <!-- <div v-for="(item, index) in items" :key="index" class="photo"> -->
+          <n-card v-for="(item, index) in items" :key="index" class="anno" :title="item?.content">
+            <template #header-extra>
+              <router-link :to="`/message/${item?.tg_id}`" class="tag-link"
+                >Go to {{ item?.tg_id }}</router-link
+              ></template
+            >
             <n-space justify="space-between">
-              <n-tag>{{ countries?.[item?.country]?.name }}</n-tag>
-              <n-button quaternary type="primary">
-                <template #icon>
-                  <n-icon
-                    :component="item?.orient > 1 ? ArrowUp : ArrowDown"
-                    :color="item?.orient > 1 ? 'red' : 'green'"
-                  />
-                </template>
-              </n-button>
-              <router-link :to="`/message/${item?.tg_id}`" class="tag-link">Go to {{ item?.tg_id }}</router-link>
+              <n-space vertical>
+                <n-space>
+                  <template v-for="tag in item.features">
+                    <n-button-group size="small" v-if="tag?.note">
+                      <n-button type="info" size="small">{{ features[tag.id]?.code?.toUpperCase() }}</n-button>
+                      <n-tooltip trigger="hover">
+                        <template #trigger>
+                          <n-button size="small" color="#2080f0">
+                            <template #icon>
+                              <n-icon :component="InfoCircle" />
+                            </template>
+                          </n-button>
+                        </template>
+                        {{ tag.note }}
+                      </n-tooltip>
+                    </n-button-group>
+                    <template v-else>
+                      <n-button v-if="features[tag.id]?.parent === 1" color="#5a428d" size="small">{{
+                        features[tag.id]?.code?.toUpperCase()
+                      }}</n-button>
+                      <n-button v-else type="info" size="small">{{ features[tag.id]?.code?.toUpperCase() }}</n-button>
+                    </template>
+                  </template>
+                </n-space>
+                <n-space justify="start">
+                  <n-button tertiary>{{ countries?.[item?.country]?.name }}</n-button>
+                  <n-button :type="item?.orient > 1 ? 'secondary' : 'primary'">
+                    <template #icon>
+                      <n-icon :component="Square" color="red" />
+                    </template>
+                  </n-button>
+                </n-space>
+              </n-space>
+              <img style="max-width: 300px" :src="'/api/media/fragments/' + item.id + '.png'" />
             </n-space>
-          </n-space>
-        </div>
+          </n-card>
+        </n-space>
       </div>
+      <n-pagination
+        v-model:page="page"
+        v-model:page-size="pageSize"
+        show-size-picker
+        :item-count="totalCount"
+        :page-sizes="paginationOptions"
+        @update:page="changePage"
+        @update:page-size="changePageSize"
+      />
     </n-space>
   </n-card>
 </template>
 <script setup lang="ts">
 import { reactive, ref, onBeforeMount } from 'vue';
 import axios from 'axios';
-import { ArrowDown, ArrowUp, InfoCircle } from '@vicons/fa';
+import { ArrowDown, ArrowUp, InfoCircle, Square } from '@vicons/fa';
 import { useRoute, onBeforeRouteUpdate } from 'vue-router';
 import router from '../router';
 
@@ -87,9 +84,10 @@ const page = ref(1);
 const pageSize = ref(50);
 const totalCount = ref(0);
 const isLoaded = ref(false);
-const items = reactive<Array<IMessage>>([]);
+const items = reactive<Array<IAnnotation>>([]);
+const features = reactive({} as keyable);
 const countries = reactive({} as keyable);
-const languages = reactive([] as Array<string>);
+const paginationOptions = [10, 50, 100, 250, 500, 1000];
 
 const vuerouter = useRoute();
 const pageIn = Number(vuerouter.params.page);
@@ -102,59 +100,6 @@ if (batchIn) {
   pageSize.value = batchIn;
 }
 
-const processAnnotations = (itemAnn: IAnnotation, id: number) => {
-  // console.log(annos);
-  const suggestions = {} as keyable;
-  const tags = [];
-  let flag = true;
-  let heading = '';
-  let error = false;
-  for (let item of itemAnn.body) {
-    if (['commenting', 'replying'].includes(item.purpose)) {
-      if (flag) {
-        heading = item.value;
-        flag = false;
-      } else {
-        const tagContent = item.value.replaceAll('\n', '');
-        const matches = tagContent.matchAll(/^(TAG-[A-Z]+)[:]?\s*(.*)$/g);
-        const results = Array.from(matches);
-        if (results?.[0]?.length) {
-          if (!results?.[0][2]) {
-            console.warn(id, results?.[0]?.length, results?.[0]);
-            error = true;
-          } else {
-            suggestions[results?.[0][1]] = results?.[0][2];
-          }
-        } else {
-          console.warn(id, item.value);
-          error = true;
-        }
-      }
-    }
-  }
-
-  for (let item of itemAnn.body) {
-    if (item.purpose === 'tagging') {
-      const obj = { label: item.value.replace('TAG-', ''), tag: item.value } as keyable;
-      const title = suggestions?.[item.value];
-      if (title) {
-        obj.title = title;
-      }
-      if (languages.includes(item.value)) {
-        obj.language = true;
-      }
-      tags.push(obj);
-    }
-  }
-
-  if (!heading) {
-    error = true;
-  }
-
-  return { ...(error && { error }), ...{ heading, tags: tags.sort(x => (x?.language ? -1 : 2)), id: itemAnn.id } };
-  // return '';
-};
-
 const updatePage = async () => {
   isLoaded.value = false;
   console.log('call update page');
@@ -163,12 +108,17 @@ const updatePage = async () => {
     params: { offset: (page.value - 1) * pageSize.value, limit: pageSize.value },
   });
   Object.assign(items, data.selection);
+
   totalCount.value = Number(data.count);
 
   ({ data } = await axios.get('/api/scheme'));
   const countriesList = Object.assign({}, ...data.countries.map((x: any) => ({ [x.code]: x })));
   Object.assign(countries, countriesList);
-  Object.assign(languages, data.languages);
+
+  ({ data } = await axios.get('/api/features'));
+  const featuresData = Object.fromEntries(data.map((x: any) => [x.id, x]));
+  Object.assign(features, featuresData);
+
   isLoaded.value = true;
 };
 
