bd604279579d163a0f4943e7b5822e243aa96940
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-15 14:40:28 +0300

    catch errors in thumbnails making script [server]

--- server/make-thumbnails.js
+++ server/make-thumbnails.js
@@ -6,8 +6,8 @@ import sharp from 'sharp';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const imagesDir = path.join(__dirname, 'media', 'downloaded');
-const thumbsDir = path.join(__dirname, 'media', 'thumbs');
+const imagesDir = path.join(__dirname, 'media', 'downloads');
+const thumbsDir = path.join(__dirname, 'media', 'thumbnails');
 
 const constraints = {
   //   width: 650,
@@ -15,8 +15,18 @@ const constraints = {
   fit: 'inside',
 };
 
+const makeThumbnail = async (file) => {
+  try {
+    const promise = await sharp(path.join(imagesDir, file)).resize(constraints).toFormat('jpg').toFile(path.join(thumbsDir, file));
+    return promise;
+  } catch (error) {
+    console.log('Image error!', file);
+    return Promise.reject(error);
+  }
+};
+
 const promises = fs.readdirSync(imagesDir)
   .filter((x) => path.extname(x) === '.jpg' && !fs.existsSync(path.join(thumbsDir, x)))
-  .map((x) => sharp(path.join(imagesDir, x)).resize(constraints).toFormat('jpg').toFile(path.join(thumbsDir, x)));
+  .map((x) => makeThumbnail(x));
 
-await Promise.all(promises);
+await Promise.allSettled(promises);
