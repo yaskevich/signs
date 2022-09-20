993d8306fbbc5ab9da19462c2743856415596e52
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-20 19:23:00 +0300

    render app info in Home [client]

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
     <div v-if="isLoaded" style="text-align: center; max-width: 350px; margin: auto">
+      <n-card :bordered="false">
       <div v-for="item in datum?.tree">
-        <n-h3> {{ item.title || item.code }} ({{ (datum as keyable)?.[item.code] }})</n-h3>
+        <n-h3> {{ item.title || item.code }} • {{ (datum as keyable)?.[item.code] }}</n-h3>
         <div v-for="item2 in item.children.filter((x:any) => x.type !== 'text')">
-          <n-h5 style="font-variant: small-caps">{{ item2.code }}</n-h5>
+          <n-h5 style="font-variant: small-caps">{{ item2.title || item2.code }}</n-h5>
           <template v-for="item3 in item2?.children?.sort((a:any, b:any) => b.num - a.num)">
             <template v-if="item3.type">
               <n-space vertical>
@@ -42,6 +43,31 @@
           <span>...with unique photos</span> <span> {{ datum?.messages?.images - datum?.messages?.dups }} </span>
         </n-space>
       </n-space>
+      <n-divider></n-divider>
+      <n-card>
+        <n-h5>Platform {{ info?.unix ? ' • ' + new Date(info.unix * 1000).toLocaleDateString() : '' }}</n-h5>
+        <n-space vertical>
+          <n-space justify="space-between">
+            <span>Client</span> <span> {{ project.version }} </span>
+          </n-space>
+          <n-space justify="space-between">
+            <span>Server</span>
+            <span>{{ info.server }}</span>
+          </n-space>
+          <n-space justify="space-between">
+            <span>Commit</span>
+            <n-button
+              text
+              tag="a"
+              :href="'https' + project?.repository?.url?.slice(3, -4) + '/commit/' + info.commit"
+              target="_blank"
+              type="primary"
+              >{{ info.commit }}</n-button
+            >
+          </n-space>
+        </n-space>
+      </n-card>
+    </n-card>
     </div>
     <div v-else style="text-align: center">...loading</div>
   </div>
@@ -50,13 +76,18 @@
 <script setup lang="ts">
 import { ref, reactive, onBeforeMount } from 'vue';
 import axios from 'axios';
+import project from '../../package.json';
 
 const isLoaded = ref(false);
 const datum = reactive({} as IStats);
+const info = ref();
 
 onBeforeMount(async () => {
   const { data } = await axios.get('/api/stats');
   Object.assign(datum, data);
+
+  const infodata = await axios.get('/api/user/info');
+  info.value = infodata.data;
   // console.log('stats', data);
   isLoaded.value = true;
 });
