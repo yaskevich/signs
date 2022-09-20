c853604e944fa4b9104f6056b69a4c8877f727f4
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-20 14:57:43 +0300

    update typings [client]

--- client/src/index.d.ts
+++ client/src/index.d.ts
@@ -1,53 +1,71 @@
 interface keyable {
-    [key: string]: any,
+  [key: string]: any;
 }
 
 interface IStats {
-    annotations: number,
-    messages: number,
-    photos: {total: number, dups: number},
-    orientation: Array<Array<number>>,
-    languages: Array<string>,
-    features: Array<string>,
-    scheme: {
-        languages: Array<string>,
-        features: Array<string>,
-        orientation: keyable,
-    },
+  annotations: number;
+  annotated: number;
+  messages: { all: number; images: number; dups: number };
+  orientation: Array<Array<number>>;
+  languages: Array<string>;
+  features: Array<string>;
+  tree: keyable;
+}
+
+interface IFeature {
+  id: number;
+  code: string;
+  value: boolean | string | number;
+  title: string;
+  note: string;
+  parent: number;
+  type: string;
+  comment: string;
+  children: Array<IFeature>;
+  ref?: IFeature
 }
 
 interface IAnnotation {
-    id: string,
-    type: string,
-    body: Array<{
-        "type": string,
-        "value": string,
-        "purpose": string,
-    }>,
+  id: string;
+  type: string;
+  content: string;
+  tg_id?: number;
+  orient: number;
+  country: string;
+  properties: Array<IFeature>;
+  features: Array<IFeature>;
 }
 
 interface IMessage {
-    tg_id: number,
-    orient: number,
-    country: string,
-    next?: number,
-    prev?: number,
-    url: string,
-    src: string,
-    created: string,
-    imagepath: string,
-    annotations: Array<IAnnotation>,
-    data: { message: string, grouped_id: string, from_id: {user_id: string}, fwd_from: { from_id: { channel_id: number }, channel_post: string, date: string, user_id: string }, date: string, views: string, "_": string, },
+  tg_id: number;
+  orient: number;
+  country: string;
+  next?: number;
+  prev?: number;
+  url: string;
+  src: string;
+  created: string;
+  imagepath: string;
+  annotated: string;
+  data: {
+    message: string;
+    grouped_id: string;
+    from_id: { user_id: string };
+    fwd_from: { from_id: { channel_id: number }; channel_post: string; date: string; user_id: string };
+    date: string;
+    views: string;
+    _: string;
+  };
 }
 
 interface IUser {
-    id: number,
-    username: string | null,
-    firstname: string | null,
-    lastname: string | null,
-    tg_id: number,
+  id: number;
+  username: string | null;
+  firstname: string | null;
+  lastname: string | null;
+  tg_id: number;
 }
 
 interface IUsersDict {
-    [id: string]: IUser,
+  [id: string]: IUser;
 }
