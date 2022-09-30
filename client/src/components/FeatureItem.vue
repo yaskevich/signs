c15089da269b4fd472645f99de15e82269789ce4
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-30 15:08:30 +0300

    create component FeatureItem [client]

---
+++ client/src/components/FeatureItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <n-tooltip trigger="hover" v-if="item.comment">
+    <template #trigger>
+      <n-text>{{ item.title || item.code.toUpperCase() }}</n-text>
+    </template>
+    {{ item.comment }}
+  </n-tooltip>
+  <n-text v-else>{{ item.title || item.code.toUpperCase() }}</n-text>
+</template>
+<script setup lang="ts">
+defineProps<{
+  item: { code: string; title: string; comment: string };
+}>();
+</script>
