593ef34593f3229b49080fe91c2dd59dc5f049b6
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-10-05 16:59:23 +0300

    require authentication for routes returning data [server]

--- server/index.js
+++ server/index.js
@@ -28,13 +28,6 @@ const mediaDir = path.join(__dirname, 'media');
 const imagesDir = path.join(mediaDir, 'downloads');
 const fragmentsDir = path.join(mediaDir, 'fragments');
 
-const createToken = (user) => jwt.sign({
-  iss: appName,
-  sub: user.id,
-  iat: new Date().getTime(),
-  exp: new Date().setDate(new Date().getDate() + 1),
-}, secret);
-
 const nest = (items, id = 0) => items
   .filter((x) => x.parent === id)
   .map((x) => {
@@ -56,12 +49,11 @@ passport.use(strategy);
 const auth = passport.authenticate('jwt', { session: false });
 const app = express();
 
-app.use('/api/media', express.static(mediaDir));
+app.use('/api/media', [auth, express.static(mediaDir)]);
 app.use(express.static('public'));
 
 app.use(compression());
 app.set('trust proxy', 1);
-app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(history());
@@ -72,16 +64,21 @@ app.use(history());
 //   ]
 // }));
 
+app.get('/api/settings', async (req, res) => res.json(await db.getSettings()));
+
 app.post('/api/user/login', async (req, res) => {
   const userData = await db.getUserData(req.body.email, req.body.password);
   if (userData && Object.keys(userData).length && !userData?.error) {
     console.log(req.body.email, '<SUCCESS>');
-    const token = createToken(userData);
-    userData.token = token;
-    userData.server = __package.version;
-    userData.commit = commit;
-    userData.unix = unix;
-    res.json(userData);
+    const token = jwt.sign({
+      iss: appName,
+      sub: userData.id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1),
+    }, secret);
+    res.json({
+      ...userData, token, server: __package.version, commit, unix
+    });
   } else {
     console.log(`login attempt as [${req.body.email}]•[${req.body.password}]►${userData.error}◄`);
     res.json(userData);
@@ -102,15 +99,17 @@ app.post('/api/user/reg', async (req, res) => {
 //   return res.send();
 // });
 
-app.get('/api/user/info', async (req, res) => {
-  res.json({ server: __package.version, commit, unix, });
+app.get('/api/user/info', auth, async (req, res) => {
+  res.json({
+    ...req.user, server: __package.version, commit, unix,
+  });
 });
 
-app.get('/api/features', async (req, res) => {
+app.get('/api/features', auth, async (req, res) => {
   res.json(await db.getFeatures());
 });
 
-app.get('/api/stats', async (req, res) => {
+app.get('/api/stats', auth, async (req, res) => {
   const [photos, messages, features, objects, astat, pstat] = await Promise.all([
     db.getMessagesAnnotatedCount(),
     db.getPhotosCount(),
@@ -126,7 +125,7 @@ app.get('/api/stats', async (req, res) => {
   });
 });
 
-app.get('/api/messages', async (req, res) => {
+app.get('/api/messages', auth, async (req, res) => {
   // console.log(req.query);
   const count = await db.getMessagesCount();
   const data = await db.getMessages(Number(req.query.off), Number(req.query.batch));
@@ -140,23 +139,23 @@ app.get('/api/messages', async (req, res) => {
   });
 });
 
-app.get('/api/objects', async (req, res) => res.json(await db.getObjects(req.query)));
+app.get('/api/objects', auth, async (req, res) => res.json(await db.getObjects(req.query)));
 
-app.get('/api/attached', async (req, res) => res.json(await db.getAttachedObjects(req.query.id)));
+app.get('/api/attached', auth, async (req, res) => res.json(await db.getAttachedObjects(req.query.id)));
 
-app.post('/api/meta', async (req, res) => res.json(await db.setPhotoMeta(req.body.params)));
+app.post('/api/meta', auth, async (req, res) => res.json(await db.setPhotoMeta(req.body.params)));
 
-app.post('/api/feature', async (req, res) => res.json(await db.updateFeature(req.body.params)));
+app.post('/api/feature', auth, async (req, res) => res.json(await db.updateFeature(req.body.params)));
 
-app.post('/api/object', async (req, res) => res.json(await db.setObject(req.body.params, imagesDir, fragmentsDir)));
+app.post('/api/object', auth, async (req, res) => res.json(await db.setObject(req.body.params, imagesDir, fragmentsDir)));
 
-app.delete('/api/object/:id', async (req, res) => res.json(await db.deleteObject(req.params.id, fragmentsDir)));
+app.delete('/api/object/:id', auth, async (req, res) => res.json(await db.deleteObject(req.params.id, fragmentsDir)));
 
-app.get('/api/message', async (req, res) => res.json(await db.getMessage(Number(req.query.id))));
+app.get('/api/message', auth, async (req, res) => res.json(await db.getMessage(Number(req.query.id))));
 
-app.get('/api/next', async (req, res) => res.json(await db.getNext(Number(req.query.id))));
+app.get('/api/next', auth, async (req, res) => res.json(await db.getNext(Number(req.query.id))));
 
-app.get('/api/prev', async (req, res) => res.json(await db.getPrev(Number(req.query.id))));
+app.get('/api/prev', auth, async (req, res) => res.json(await db.getPrev(Number(req.query.id))));
 
 app.listen(port);
 console.log(`Backend is at port ${port}`);
