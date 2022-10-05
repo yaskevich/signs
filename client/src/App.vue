443676a6259893c4535580c681ee7d87af3aedad
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-10-05 17:32:36 +0300

    handle user's authenticated state in App [client]

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,61 @@
 <template>
-  <n-layout position="absolute">
-    <n-layout-header id="nav">
-      <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
-    </n-layout-header>
-    <n-layout-content style="max-width: 900px; min-height: 500px; margin: auto">
-      <n-message-provider>
-        <router-view :key="$route.fullPath" />
-      </n-message-provider>
-    </n-layout-content>
-    <n-layout-footer style="margin: 1rem; padding: 1rem">
-      <n-space justify="center">
-        <n-button text tag="a" href="https://yaskevich.com/" target="_blank">
-          <template #icon>
-            <n-icon :component="Camera" />
-          </template>
-          2020–2022 •&nbsp;<strong>Signs</strong>&nbsp;by Alyaxey Yaskevich
-        </n-button>
-      </n-space>
-    </n-layout-footer>
-  </n-layout>
+  <n-message-provider>
+    <template id="main" v-if="loggedIn">
+      <n-layout position="absolute">
+        <n-layout-header id="nav">
+          <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" @update:value="processMenu" />
+        </n-layout-header>
+        <n-layout-content style="max-width: 900px; min-height: 500px; margin: auto">
+          <router-view :key="$route.fullPath" />
+        </n-layout-content>
+        <n-layout-footer style="margin: 1rem; padding: 1rem">
+          <n-space justify="center">
+            <n-button text tag="a" href="https://yaskevich.com/" target="_blank">
+              <template #icon>
+                <n-icon :component="Camera" />
+              </template>
+              2020–2022 •&nbsp;<strong>Signs</strong>&nbsp;by Alyaxey Yaskevich
+            </n-button>
+          </n-space>
+        </n-layout-footer>
+      </n-layout>
+    </template>
+    <div v-else style="max-width: 300px; margin: auto">
+      <n-tabs
+        default-value="signin"
+        size="large"
+        animated
+        justify-content="center"
+        style="margin: 0 -4px"
+        pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;">
+        <n-tab-pane name="signin" tab="Log in">
+          <Login />
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="Register">
+          <Register v-if="settings?.registration_open" />
+          <n-h2 v-else>
+            <n-text type="warning">Registration is closed.</n-text>
+          </n-h2>
+        </n-tab-pane>
+      </n-tabs>
+    </div>
+  </n-message-provider>
 </template>
 <script setup lang="ts">
-import { ThumbsUpRegular, Camera, Bars, Faucet, ObjectGroupRegular, Sitemap, Home } from '@vicons/fa';
+import { ThumbsUpRegular, Camera, Bars, Faucet, ObjectGroupRegular, Sitemap, Home, User, SignInAlt } from '@vicons/fa';
 import router from './router';
 import { RouterLink, useRoute } from 'vue-router';
 import { h, Component, ref, reactive, onBeforeMount, onMounted, computed, watch } from 'vue';
 import { MenuOption, NIcon } from 'naive-ui';
+import store from './store';
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
 
 const vuerouter = useRoute();
 const activeKey = ref<string | null>(null); // vuerouter?.name||'Home'
+const loggedIn = computed(() => store?.state?.token?.length);
+const settings = ref<ISettings>();
+const username = ref(String(store?.state?.user?.username || ''));
 
 const renderIcon = (icon: Component) => {
   return () => h(NIcon, null, { default: () => h(icon) });
@@ -40,15 +67,42 @@ const makeItem = (name: string, title: string, icon: Component) => ({
   icon: renderIcon(icon),
 });
 
+const processMenu = (key: string, item: MenuOption) => {
+  if (key === 'logout') {
+    store.logoutUser();
+  }
+};
+
 const menuOptions: MenuOption[] = reactive([
   makeItem('Home', 'Home', Home),
   makeItem('TMessages', 'Input', Faucet),
   makeItem('Flow', 'Objects', ObjectGroupRegular),
   makeItem('Features', 'Features', Sitemap),
+  {
+    label: username,
+    key: 'username',
+    disabled: false,
+    icon: renderIcon(User),
+    children: [
+      {
+        label: 'Log out',
+        key: 'logout',
+        disabled: false,
+        icon: renderIcon(SignInAlt),
+      },
+    ],
+  },
 ]);
 
 onMounted(async () => {
-  activeKey.value = String(vuerouter.name);
+  await store.getUser();
+  if (store?.state?.user?.username) {
+    activeKey.value = String(vuerouter.name);
+    username.value = String(store?.state?.user?.username);
+  } else {
+    const { data } = await store.getUnauthorized('settings');
+    settings.value = data;
+  }
 });
 </script>
 
@@ -68,7 +122,8 @@ onMounted(async () => {
     font-weight: bold;
     color: #2c3e50;
 
-    &.router-link-exact-active, &.router-link-active {
+    &.router-link-exact-active,
+    &.router-link-active {
       color: #42b983;
     }
   }
