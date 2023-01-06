a2fc0b253189d08c6a09c82fea7f9851638018ca
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-01-06 23:51:42 +0300

    create component Settings [client]

---
+++ client/src/components/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-card title="Settings" :bordered="false" class="minimal left">
+    {{ settings }}
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, onBeforeMount } from 'vue';
+import store from '../store';
+import { useMessage } from 'naive-ui';
+const message = useMessage();
+const settings = ref();
+
+onBeforeMount(async () => {
+  const { data } = await store.getUnauthorized('settings');
+  settings.value = data;
+});
+</script>
