9c88097c404ac6ef0109627da4ba16548314cffc
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-01-13 20:58:52 +0300

    enable vue-json-viewer [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import './registerServiceWorker'
 import router from './router'
+import JsonViewer from 'vue-json-viewer'
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(JsonViewer).mount('#app');
--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -10,5 +10,24 @@ declare module '@recogito/annotorious' {
   }
 }
 declare module '@recogito/annotorious-tilted-box' {
-  export default function TiltedBoxPlugin(T:Object):any
+  export default function TiltedBoxPlugin(T: Object): any;
+}
+declare module 'vue-json-viewer' {
+  import { AllowedComponentProps, App, Component, ComponentCustomProps, VNodeProps } from 'vue';
+  interface JsonViewerProps {
+    value: Object | Array<any> | string | number | boolean;
+    expanded: boolean;
+    expandDepth: number;
+    copyable: boolean | object;
+    sort: boolean;
+    boxed: boolean;
+    theme: string;
+    previewMode: boolean;
+    timeformat: (value: any) => string;
+  }
+  type JsonViewerType = JsonViewerProps & VNodeProps & AllowedComponentProps & ComponentCustomProps;
+  const JsonViewer: Component<JsonViewerType>;
+  export { JsonViewer };
+  const def: { install: (app: App) => void };
+  export default def;
 }
