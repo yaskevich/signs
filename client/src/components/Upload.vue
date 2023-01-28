14ac4e4f7c942d3b51edb43b48ad31e3d7dcf496
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-01-28 23:36:19 +0300

    handle single/multiple mode in Upload [client]

--- client/src/components/Upload.vue
+++ client/src/components/Upload.vue
@@ -1,35 +1,74 @@
 <template>
   <n-card title="Upload" :bordered="false" class="minimal left">
-    <n-upload multiple directory-dnd action="/api/upload/" :headers="headers" 
-    @before-upload="beforeUpload" @finish="imageLoaded">
-      <n-upload-dragger>
-        <div style="margin-bottom: 12px">
-          <!-- <n-button size="small" color="#2080f0">
+    <template #header-extra>
+      <n-tooltip trigger="hover">
+        <template #trigger>
+          <n-switch :rail-style="railStyle" v-model:value="mode">
+            <template #checked> Multiple Files</template>
+            <template #unchecked> Single File </template>
+          </n-switch>
+        </template>
+        After single file upload a user will be redirected to annotation screen. <br />
+        In multiple mode a user will stay on upload screen.
+      </n-tooltip>
+    </template>
+    <n-space vertical>
+      <n-upload
+        :multiple="mode"
+        directory-dnd
+        action="/api/upload/"
+        :headers="headers"
+        @before-upload="beforeUpload"
+        @finish="imageLoaded">
+        <n-upload-dragger>
+          <div style="margin-bottom: 12px">
+            <!-- <n-button size="small" color="#2080f0">
           <template #icon>  -->
-          <n-icon size="48" color="green" :component="FileUploadFilled" />
-          <!-- </template>
+            <n-icon size="48" color="green" :component="FileUploadFilled" />
+            <!-- </template>
         </n-button> -->
 
-          <!-- <n-icon size="48" :depth="3">
+            <!-- <n-icon size="48" :depth="3">
             <archive-icon />
           </n-icon> -->
-        </div>
-        <n-text style="font-size: 16px"> Click or drag one or multiple image files to this area to upload </n-text>
-        <n-p depth="3" style="margin: 8px 0 0 0">
-          Only PNG and JPEG formats are accepted. Upload the file version of the best quality you have.
-        </n-p>
-      </n-upload-dragger>
-    </n-upload>
+          </div>
+          <n-text style="font-size: 16px"> Click or drag one or multiple image files to this area to upload </n-text>
+          <n-p depth="3" style="margin: 8px 0 0 0">
+            Only PNG and JPEG formats are accepted. Upload the file version of the best quality you have.
+          </n-p>
+        </n-upload-dragger>
+      </n-upload>
+    </n-space>
   </n-card>
 </template>
 
 <script setup lang="ts">
 import { FileUploadFilled } from '@vicons/material';
+import { CSSProperties, ref } from 'vue';
 import store from '../store';
 import type { UploadInst, UploadFileInfo, MessageType } from 'naive-ui';
 import { useMessage } from 'naive-ui';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const message = useMessage();
+const mode = ref(false);
+
+const railStyle = ({ focused, checked }: { focused: boolean; checked: boolean }) => {
+  const style = {} as CSSProperties;
+  if (checked) {
+    style.background = '#d03050';
+    if (focused) {
+      style.boxShadow = '0 0 0 2px #d0305040';
+    }
+  } else {
+    style.background = '#2080f0';
+    if (focused) {
+      style.boxShadow = '0 0 0 2px #2080f040';
+    }
+  }
+  return style;
+};
 
 const headers = { Authorization: 'Bearer ' + store.state.token };
 // const previewFileList = reactive<CustomFileInfo[]>([]);
@@ -40,17 +79,19 @@ const beforeUpload = (data: { file: UploadFileInfo; fileList: UploadFileInfo[] }
     return false;
   }
   console.log(data);
-  
   return true;
 };
 
 const imageLoaded = (options: { file: UploadFileInfo; event?: Event }) => {
-  // console.log("options", options, previewFileList);
+  // console.log("options", options);
   // console.log('loaded', options.file);
-
   if (options?.file?.status === 'finished') {
-    if (options?.event?.target){
-        console.log('done', JSON.parse((options.event.target as XMLHttpRequest).response));
+    if (options?.event?.target) {
+      const datum = JSON.parse((options.event.target as XMLHttpRequest).response);
+      console.log('done', datum);
+      if (!mode.value) {
+        router.push({ name: 'Toolset', params: { id: datum?.id } });
+      }
     }
     // const path = (options?.event?.target as XMLHttpRequest)?.responseText;
     // const url = `/api/images/${id}/${path}`;
@@ -58,7 +99,6 @@ const imageLoaded = (options: { file: UploadFileInfo; event?: Event }) => {
     // previewFileList.unshift({ ...options.file, url, title: options.file.name.split('.').slice(0, -1).join('.') });
     // loadedFiles[options.file.id] = path;
   }
-
   return;
 };
 </script>
