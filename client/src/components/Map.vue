6d936879acba88803572c3c0df19029433361a8a
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-06-16 22:41:24 +0200

    create component Map [client]

---
+++ client/src/components/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-card title="Map" :bordered="false" class="minimal" v-if="isLoaded">
+    {{ datum }}
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, onBeforeMount } from 'vue';
+import store from '../store';
+const isLoaded = ref(false);
+const datum = ref([]);
+
+onBeforeMount(async () => {
+  datum.value = await store.get('map');
+  isLoaded.value = true;
+});
+</script>
