35b3723abebb275afd4739ad80382dc4e212b91a
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-06-23 22:58:53 +0200

    render map in Mapper [client]

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,17 +1,19 @@
 <template>
   <n-card title="Map" :bordered="false" class="minimal" v-if="isLoaded">
-    {{ datum }}
+    <Mapper :data="data" />
   </n-card>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, onBeforeMount } from 'vue';
 import store from '../store';
+import Mapper from './Mapper.vue';
+
 const isLoaded = ref(false);
-const datum = ref([]);
+const data = ref([]);
 
 onBeforeMount(async () => {
-  datum.value = await store.get('map');
+  data.value = await store.get('map');
   isLoaded.value = true;
 });
 </script>
