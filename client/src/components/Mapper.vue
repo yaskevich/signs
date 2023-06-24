fb828711e95daefdc751c456ee083ad8e1da2135
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-06-24 19:24:09 +0200

    render popup for points in Mapper [client]

--- client/src/components/Mapper.vue
+++ client/src/components/Mapper.vue
@@ -8,7 +8,14 @@
 import { h, ref, watch, onUnmounted, onMounted, markRaw, shallowRef, toRaw } from 'vue';
 import type { Position, Point, FeatureCollection, Feature } from 'geojson';
 import { Map, NavigationControl, Marker, Popup, FullscreenControl, LngLatBounds } from 'maplibre-gl';
-import type { GeoJSONSource, StyleSpecification, ResourceTypeEnum, MapOptions, LngLatLike } from 'maplibre-gl';
+import type {
+  GeoJSONSource,
+  StyleSpecification,
+  ResourceTypeEnum,
+  MapOptions,
+  LngLatLike,
+  LngLatBoundsLike,
+} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { isMapboxURL, transformMapboxUrl } from 'maplibregl-mapbox-request-transformer';
 import store from '../store';
@@ -20,7 +27,7 @@ const props = withDefaults(
   }>(),
   {}
 );
-console.log('setup', props);
+// console.log('setup', props);
 
 const init = ref(false);
 const mapContainer = ref<HTMLElement>();
@@ -28,9 +35,8 @@ const map = shallowRef<Map>();
 const showMap = ref(false);
 const marker = shallowRef<Marker>();
 
-const initMap = (lngLat: [number, number], geo: any) => {
-  console.log('init map');
-
+const initMap = (lngLat: LngLatLike, geo: any) => {
+  // console.log('init map');
   const {
     map_vector: isVector,
     map_tile: tilePath,
@@ -38,7 +44,6 @@ const initMap = (lngLat: [number, number], geo: any) => {
     map_mapbox: isMapbox,
     map_mapbox_key: mapboxKey,
   } = store?.state?.user?.settings as IUser['settings'];
-  console.log(geo);
 
   const tileServer = !isVector && tilePath ? tilePath : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
@@ -67,10 +72,21 @@ const initMap = (lngLat: [number, number], geo: any) => {
   const style = isVector ? stylePath : rasterStyle;
 
   if (style) {
+    let center = lngLat;
+    let bounds: LngLatBoundsLike;
+    if (geo) {
+      const coordinates = geo.features.map((x: Feature) => (x?.geometry as Point)?.coordinates);
+      bounds = coordinates.reduce(
+        (bound: any, coord: Position) => bound.extend(coord),
+        new LngLatBounds(coordinates[0], coordinates[0])
+      );
+      center = (bounds as LngLatBounds).getCenter();
+    }
+
     const mapSetup = {
       container: mapContainer.value,
       style,
-      center: lngLat || geo?.features?.[0].geometry?.coordinates,
+      center: center,
       zoom: 12,
     } as MapOptions;
 
@@ -108,27 +124,70 @@ const initMap = (lngLat: [number, number], geo: any) => {
             // type: 'symbol',
             type: 'circle',
             source: 'points-source',
+            paint: {
+              'circle-radius': 10,
+              // 'circle-color': 'transparent',
+              'circle-stroke-color': 'yellow',
+              'circle-stroke-width': 2,
+              'circle-color': 'blue',
+            },
           },
           firstSymbolId
         );
-
-        const coordinates = geo.features.map((x: Feature) => (x?.geometry as Point)?.coordinates);
-        const bounds = coordinates.reduce(
-          (bound: any, coord: Position) => bound.extend(coord),
-          new LngLatBounds(coordinates[0], coordinates[0])
-        );
-        console.log(bounds);
-
-        map.fitBounds(bounds, { padding: 50, animate: false  });
+        map.fitBounds(bounds, { padding: 50, animate: false });
       }
     });
+    map.on('click', 'points-layer', function (e) {
+      if (e?.features?.length) {
+        // console.log(e.features[0]?.properties?.id, e.features[0]?.properties);
+        console.log(e.features[0]?.properties?.content, e.features?.[0]?.properties?.features);
+        //   const point = geo.features.find((x: Feature) => x.properties?.id === e?.features?.[0]?.properties?.id);
+        //   if (point?.properties?.id) {
+        //     item.value = point;
+        //     showModal.value = true;
+        //   }
+      }
+    });
+
+    const popup = new Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+    map.on('mouseenter', 'points-layer', function (e) {
+      map.getCanvas().style.cursor = 'pointer';
+      const coordinates = (e.features?.[0]?.geometry as Point)?.coordinates?.slice();
+      const description = e?.features?.[0]?.properties?.content || '🗅';
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      popup
+        .setLngLat(coordinates as LngLatLike)
+        .setHTML(description)
+        .addTo(map);
+    });
+
+    map.on('mouseleave', 'points-layer', function () {
+      map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
+
+    // map.on('mousemove', 'points-layer', function (e) {
+    //   if (e.features.length > 0) {
+    //     console.log(e.features[0].properties.content, e.features[0].properties.features);
+    // if (hoveredStateId) {
+    //   map.setFeatureState({ source: 'states', id: hoveredStateId }, { hover: false });
+    // }
+    // hoveredStateId = e.features[0].id;
+    // map.setFeatureState({ source: 'states', id: hoveredStateId }, { hover: true });
+    //   }
+    // });
 
     return map;
   }
 };
 
 const renderMap = (options: any) => {
-  console.log('render map', options);
+  // console.log('render map', options);
   let result = false;
 
   if (store?.state?.user?.settings) {
@@ -177,5 +236,10 @@ onUnmounted(() => {
   position: absolute;
   width: 100%;
   height: 100%;
+}
+
+.maplibregl-popup {
+  max-width: 400px;
+  font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
 }
 </style>
