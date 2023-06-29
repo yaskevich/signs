7c751d45b72a48ba8946ee8cc738dc47ffa034c1
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-06-29 22:21:29 +0200

    update VIte config [client]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import UnheadVite from '@unhead/addons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnheadVite(),
     VitePWA({
       registerType: 'autoUpdate', devOptions: {
         enabled: true
@@ -29,7 +31,7 @@ export default defineConfig({
     })
   ],
   server: {
-    port: 3040,
+    port: 3040, 
     proxy: {
       '/api': {
         target: 'http://localhost:3041',
