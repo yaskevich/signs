f4b2d143303165b7063770e7d5a099cf414e3c0b
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-07-01 14:06:09 +0200

    enable @vueuse/head [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createHead } from '@vueuse/head';
+import App from './App.vue';
 // import './registerServiceWorker'
-import router from './router'
-import JsonViewer from 'vue-json-viewer'
+import router from './router';
+import JsonViewer from 'vue-json-viewer';
 
-createApp(App).use(router).use(JsonViewer).mount('#app');
+createApp(App).use(createHead()).use(router).use(JsonViewer).mount('#app');
