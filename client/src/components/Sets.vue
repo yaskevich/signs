29d5850c51d4ec038f05bf17fac1df84cddaac13
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-05-12 22:24:13 +0200

    create component Sets [client]

---
+++ client/src/components/Sets.vue
@@ -0,0 +1,19 @@
+<template>
+    <n-card title="Export" :bordered="false" class="minimal" v-if="isLoaded">
+      <n-text>Exported projects</n-text>
+    </n-card>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import store from '../store';
+  
+  const isLoaded = ref(false);
+  const data = ref([]);
+  
+  onBeforeMount(async () => {
+    // data.value = await store.get('map');
+    isLoaded.value = true;
+  });
+  </script>
+  
